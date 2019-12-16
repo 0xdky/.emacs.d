3e3e323524504ccb09f3da6bbfc837e2149745de
Dhruva Krishnamurthy <dhruvakm@gmail.com>
2019-12-15 19:50:26 -0800

    Streamline coding style and make it compatible with clang-format (c-guess)

--- init.el
+++ init.el
@@ -134,12 +134,6 @@
 (use-package go-mode
   :ensure t
   :defer nil
-  :bind (
-	 ;; If you want to switch existing go-mode bindings to use lsp-mode/gopls instead
-	 ;; uncomment the following lines
-	 ;; ("C-c C-j" . lsp-find-definition)
-	 ;; ("C-c C-d" . lsp-describe-thing-at-point)
-	 )
   :hook ((go-mode . lsp-deferred)
 	 (before-save . lsp-format-buffer)
 	 (before-save . lsp-organize-imports)))
@@ -170,7 +164,7 @@
 (use-package ccls
   :ensure t
   :config
-  (setq ccls-executable "ccls"
+  (setq ccls-executable "clangd"
 	lsp-prefer-flymake nil
 	lsp-enable-snippet nil
 	lsp-enable-file-watchers nil
@@ -178,29 +172,104 @@
   :hook ((c-mode c++-mode objc-mode) .
 	 (lambda () (require 'ccls) (lsp))))
 
-;; (mapcar (lambda (mode)
-;;	  (define-key mode [(control ?t)] 'pop-tag-mark)
-;;	  (define-key mode [(control ?\])] 'lsp-find-definition)
-;;	  (define-key mode [(control ?r)] 'lsp-find-references))
-;;	(list lsp-mode-map))
-
-;; Set up before-save hooks to format buffer and add/delete imports.
-;; Make sure you don't have other gofmt/goimports hooks enabled.
-(defun lsp-go-install-save-hooks ()
-  (add-hook 'before-save-hook #'lsp-format-buffer t t)
-  (add-hook 'before-save-hook #'lsp-organize-imports t t))
-(add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
-
 (require 'server)
 (setq server-use-tcp t)
 (if (not (server-running-p)) (server-start))
 
 ;; Programming
-(setq c-default-style "linux")
+;;-----------------------------------------------------------------------------
+;; Local C/C++ style
+;; clang-format -style="{BasedOnStyle: llvm, IndentWidth: 4, AccessModifierOffset: -4}" -dump-config
+;;-----------------------------------------------------------------------------
+(c-add-style "dky"
+	     '("gnu"
+	       (c-basic-offset . 4)	; Guessed value
+	       (c-offsets-alist
+		(access-label . 0)	; Guessed value
+		(block-close . 0)	; Guessed value
+		(class-close . 0)	; Guessed value
+		(cpp-define-intro . +)	; Guessed value
+		(defun-block-intro . +)	; Guessed value
+		(defun-close . 0)	; Guessed value
+		(inclass . +)		; Guessed value
+		(inline-close . 0)	; Guessed value
+		(member-init-intro . +)	; Guessed value
+		(statement . 0)		; Guessed value
+		(statement-block-intro . +) ; Guessed value
+		(stream-op . 5)		; Guessed value
+		(topmost-intro . 0)	; Guessed value
+		(annotation-top-cont . 0)
+		(annotation-var-cont . +)
+		(arglist-close . c-lineup-close-paren)
+		(arglist-cont c-lineup-gcc-asm-reg 0)
+		(arglist-cont-nonempty . c-lineup-arglist)
+		(arglist-intro . c-lineup-arglist-intro-after-paren)
+		(block-open . 0)
+		(brace-entry-open . 0)
+		(brace-list-close . 0)
+		(brace-list-entry . 0)
+		(brace-list-intro first c-lineup-2nd-brace-entry-in-arglist c-lineup-class-decl-init-+ +)
+		(brace-list-open . +)
+		(c . c-lineup-C-comments)
+		(case-label . 0)
+		(catch-clause . 0)
+		(class-open . 0)
+		(comment-intro . c-lineup-comment)
+		(composition-close . 0)
+		(composition-open . 0)
+		(cpp-macro . -1000)
+		(cpp-macro-cont . +)
+		(defun-open . 0)
+		(do-while-closure . 0)
+		(else-clause . 0)
+		(extern-lang-close . 0)
+		(extern-lang-open . 0)
+		(friend . 0)
+		(func-decl-cont . +)
+		(incomposition . +)
+		(inexpr-class . +)
+		(inexpr-statement . +)
+		(inextern-lang . +)
+		(inher-cont . c-lineup-multi-inher)
+		(inher-intro . +)
+		(inlambda . 0)
+		(inline-open . 0)
+		(inmodule . +)
+		(innamespace . +)
+		(knr-argdecl . 0)
+		(knr-argdecl-intro . 5)
+		(label . 0)
+		(lambda-intro-cont . +)
+		(member-init-cont . c-lineup-multi-inher)
+		(module-close . 0)
+		(module-open . 0)
+		(namespace-close . 0)
+		(namespace-open . 0)
+		(objc-method-args-cont . c-lineup-ObjC-method-args)
+		(objc-method-call-cont c-lineup-ObjC-method-call-colons c-lineup-ObjC-method-call +)
+		(objc-method-intro .
+				   [0])
+		(statement-case-intro . +)
+		(statement-case-open . +)
+		(statement-cont . +)
+		(string . -1000)
+		(substatement . +)
+		(substatement-label . 0)
+		(substatement-open . +)
+		(template-args-cont c-lineup-template-args +)
+		(topmost-intro-cont first c-lineup-topmost-intro-cont c-lineup-gnu-DEFUN-intro-cont))))
+
 (delete-selection-mode +1)
 (global-display-fill-column-indicator-mode +1)
+
+;; Hooks
+(add-hook 'lsp-mode-hook 'which-func-mode)
 (add-hook 'before-save-hook 'whitespace-cleanup)
 (add-hook 'find-file-hook 'vc-registered-readonly)
+(add-hook 'c-mode-common-hook (lambda ()
+				(define-key c-mode-base-map "\C-q" 'comment-region)
+				(define-key c-mode-base-map "\C-uq" 'uncomment-region)
+				(c-set-style "dky")))
 
 ;; Useful key bindings
 (global-set-key [f2] 'save-buffer)
